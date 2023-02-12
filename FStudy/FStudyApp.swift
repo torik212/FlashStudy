1eee48d9ff1231e33f41622c1294d7e780cabd02
torik212 <93282209+torik212@users.noreply.github.com>
2023-02-12 14:03:31 -0500

    Initial Commit

---
+++ FStudy/FStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FStudyApp.swift
+//  FStudy
+//
+//  Created by Tori Kestel on 2/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
